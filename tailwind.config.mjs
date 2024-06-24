a0b663af1f28f271ca16933825ad67009d9c0298
Chente-code <vicente2001.zamora@gmail.com>
2024-06-24 16:38:19 -0500

    WEB FINISH V1 🥳

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	  extend: {
+		animation: {
+		  pulse: 'pulse 2s infinite',
+		},
+		keyframes: {
+		  pulse: {
+			'0%, 100%': { transform: 'scale(1)' },
+			'50%': { transform: 'scale(1.05)' },
+		  }
+		}
+	  },
 	},
 	plugins: [],
-}
+  }
+  
